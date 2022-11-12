32ef846f7219ad0ab55cda19690d8b2999534d2c
recalderon <rc.calderon30@gmail.com>
2022-11-12 02:26:40 -0300

    remove nate

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,13 +1,2 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    var myModal = new bootstrap.Modal(document.getElementById("myModal"), {});
-        document.onreadystatechange = function () {
-        myModal.show();
-        let aceitamusica = document.querySelector('button#play');
-        let musicadeemo = document.querySelector('audio');
-        aceitamusica.addEventListener('click', (event) =>{
-            musicadeemo.play();
-            myModal.hide();
-        })
-
-    };
 })
