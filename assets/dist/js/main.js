9d3a17aeac9694d3f1c4bd10f13861ce55e20759
recalderon <rc.calderon30@gmail.com>
2022-11-13 19:30:02 -0300

    ta quase la

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -32,16 +32,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
     })
-    // const players = Array.from(document.querySelectorAll('audio')).map((p) => new Plyr(p, {
-    //     controls: [
-    //         'restart',
-    //         'play',
-    //         'progress',
-    //         'current-time',
-    //         'duration',
-    //         'mute',
-    //         'volume',
-    //     ]
-    // }));
+
+    let hasTouchScreen = false;
+    if ("maxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.maxTouchPoints > 0;
+    } else if ("msMaxTouchPoints" in navigator) {
+    hasTouchScreen = navigator.msMaxTouchPoints > 0;
+    } else {
+    const mQ = matchMedia?.("(pointer:coarse)");
+    if (mQ?.media === "(pointer:coarse)") {
+        hasTouchScreen = !!mQ.matches;
+    } else if ("orientation" in window) {
+        hasTouchScreen = true; // deprecated, but good fallback
+    } else {
+        // Only as a last resort, fall back to user agent sniffing
+        const UA = navigator.userAgent;
+        hasTouchScreen =
+        /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+        /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA);
+    }
+    }
+
+    if (hasTouchScreen) {
+        document.getElementById("play").classList.add('disabled')
+    }
 
 })
