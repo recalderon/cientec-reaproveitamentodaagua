8c959a277695502ad7e7568401660f09d95deeb5
recalderon <rc.calderon30@gmail.com>
2022-11-13 11:02:05 -0300

    ta quase la

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,3 +1,46 @@
 impress().init();
 document.addEventListener('DOMContentLoaded', (event) => {
+
+    document.addEventListener('impress:stepenter', (event) =>{
+        let targetzao = event.target
+        let stepaudio = targetzao.querySelector('.narracao')
+
+        if (stepaudio){
+            const player = new Plyr(stepaudio, {
+                resetOnEnd: true,
+                controls: [
+                    'restart',
+                    'play',
+                    'progress',
+                    'current-time',
+                    'duration',
+                    'mute',
+                    'volume',
+                ]
+            });
+            player.play();
+        }
+
+    })
+    document.addEventListener('impress:stepleave', (event) =>{
+        console.log(event)
+        let targetzao = event.target
+        let stepaudio = targetzao.querySelector('audio')
+        if (stepaudio){
+            stepaudio.pause();
+        }
+
+    })
+    // const players = Array.from(document.querySelectorAll('audio')).map((p) => new Plyr(p, {
+    //     controls: [
+    //         'restart',
+    //         'play',
+    //         'progress',
+    //         'current-time',
+    //         'duration',
+    //         'mute',
+    //         'volume',
+    //     ]
+    // }));
+
 })
